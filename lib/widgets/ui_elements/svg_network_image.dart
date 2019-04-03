0ac700adb7a21a8c5ec787aa1a15e0319833defe
IlyaUsikov <IlyaUsikov@coherentsolutions.com>
2019-04-03 17:27:46 +0300

    fix size of speaker images

--- lib/widgets/ui_elements/svg_network_image.dart
+++ lib/widgets/ui_elements/svg_network_image.dart
@@ -8,18 +8,13 @@ class SvgNetworkImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 1.0),
-      child: new Center(
-        child: new SvgPicture.network(
-          url,
-          semanticsLabel: 'Avatar',
-          width: 300.0,
-          placeholderBuilder: (BuildContext context) => new Container(
-              padding: const EdgeInsets.all(30.0),
-              child: const CircularProgressIndicator()),
-        ),
-      ),
+    return new SvgPicture.network(
+      url,
+      semanticsLabel: 'Avatar',
+      width: 300.0,
+      placeholderBuilder: (BuildContext context) => new Container(
+          padding: const EdgeInsets.all(30.0),
+          child: const CircularProgressIndicator()),
     );
   }
 }
